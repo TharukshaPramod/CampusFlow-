3a3d7005500a6010070f4cf4111330d96d7c5270
Tharuksha Pramod <tharuksha@gmail.com>
2026-03-03 18:44:27 +0530

    project started with spring initializer.

---
+++ backend/campusflow/src/main/java/com/sliit/campusflow/CampusflowApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.campusflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusflowApplication.class, args);
+	}
+
+}
